1fc70c8f6537079f430fcad5b7f59f74cb57fce4
thomas.lambert <thomas.lambert@swarmfarm.com>
2023-08-27 16:12:58 +1000

    added nav item and controller

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using EdInstitution.Models;
+
+namespace EdInstitution.Controllers;
+
+public class CourseController : Controller
+{
+    private readonly ILogger<CourseController> _logger;
+
+    public CourseController(ILogger<CourseController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public string wobbledonk() => "touch me wobbledonk";
+
+
+    ///Home/Welcome?name=booz&numTimes=77
+    public string Welcome(string name, int numTimes) => $"hello {name}, Numtimes is {numTimes}.";
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
